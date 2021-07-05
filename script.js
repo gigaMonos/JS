3b39dec6fcc3122cf756ab9cdf4b989c2ab45bae
gigaMonos <yunona.mern@gmail.com>
2021-07-05 20:29:36 +0300

    lesoon

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log('Hello world!');
+console.log('lesson' + 'JS')
+let num = 4;
+console.log('lesson' + 'JS');
+/*
+function name (){
+	console.log('lesson')
+	console.log('JS');
+}
+name();
+*/
+// comments
